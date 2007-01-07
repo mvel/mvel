d322223917b7873bd3b24a17edcc5a6385cf54d8
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2007-01-07 23:53:01 +0000

    Performance enhancements

--- src/main/java/org/mvel/compiled/FieldAccessor.java
+++ src/main/java/org/mvel/compiled/FieldAccessor.java
@@ -1,7 +1,6 @@
 package org.mvel.compiled;
 
 import org.mvel.AccessorNode;
-import org.mvel.PropertyAccessException;
 import org.mvel.integration.VariableResolverFactory;
 
 import java.lang.reflect.Field;
@@ -12,17 +11,13 @@ public class FieldAccessor implements AccessorNode {
     private Field field;
 
     public Object getValue(Object ctx, Object elCtx, VariableResolverFactory vars) throws Exception {
-        try {
-            if (nextNode != null) {
-                return nextNode.getValue(field.get(ctx), elCtx, vars);
-            }
-            else {
-                return field.get(ctx);
-            }
+        if (nextNode != null) {
+            return nextNode.getValue(field.get(ctx), elCtx, vars);
         }
-        catch (Exception e) {
-            throw new PropertyAccessException("failed to access field <<" + field.getDeclaringClass().getName() + "." + field.getName() + ">> in: " + ctx.getClass(), e);
+        else {
+            return field.get(ctx);
         }
+
     }
 
     public Field getField() {
