0cb77555afa0e142ce6871ca1becda7dd1487e48
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2007-01-15 03:34:04 +0000

    Checking in first portions of the AST Optimizer.

--- java/org/mvel/MVEL.java
+++ java/org/mvel/MVEL.java
@@ -1,7 +1,12 @@
 package org.mvel;
 
+import org.mvel.integration.VariableResolverFactory;
+import org.mvel.optimizers.ExecutableStatement;
+import org.mvel.optimizers.OptimizerFactory;
+
 public class MVEL {
-    static boolean THREAD_SAFE = Boolean.getBoolean("mvflex.expression.threadsafety");
+    static boolean THREAD_SAFE = Boolean.getBoolean("mvel.threadsafety");
+    static boolean USE_OPTIMIZER = Boolean.getBoolean("mvel.optimizer");
 
     /**
      * Force MVEL to use thread-safe caching.  This can also be specified enivromentally using the
@@ -16,8 +21,25 @@ public class MVEL {
         ExpressionParser.configureFactory();
     }
 
+    public static void setUseOptimizer(boolean optimizer) {
+        USE_OPTIMIZER = optimizer;
+    }
+
     public static boolean isThreadSafe() {
         return THREAD_SAFE;
+    }
+
+    public static boolean useOptimizer() {
+        return USE_OPTIMIZER;
+    }
+
+    public static Object execute(CompiledExpression compiledStatement, Object rootContext, VariableResolverFactory factory) {
+       return null;
+    }
+
+
+    public static ExecutableStatement optimize(TokenIterator tokenIterator, Object rootContext, VariableResolverFactory factory) {
+        return OptimizerFactory.getDefaultOptimizer().optimize(tokenIterator, rootContext, factory);
     }
 
 }
---
+++ java/org/mvel/optimizers/impl/ast/ASTOptimizer.java
@@ -0,0 +1,40 @@
+package org.mvel.optimizers.impl.ast;
+
+import org.mvel.Token;
+import org.mvel.TokenIterator;
+import org.mvel.integration.VariableResolverFactory;
+import org.mvel.optimizers.ExecutableStatement;
+import org.mvel.optimizers.Optimizer;
+import org.mvel.optimizers.impl.ast.res.Property;
+
+
+public class ASTOptimizer implements Optimizer {
+    ASTStatement astStatement = new ASTStatement();
+
+
+    public ExecutableStatement optimize(TokenIterator tokenIterator, Object staticContext, VariableResolverFactory factory) {
+        Token token;
+
+        ASTNode currentNode;
+
+        while (tokenIterator.hasMoreTokens()) {
+            token = tokenIterator.nextToken();
+
+            if (token.isIdentifier()) {
+                if (!token.isOptimized()) {
+                    token.optimizeAccessor(staticContext, factory);
+                }
+
+                currentNode = astStatement.addASTNode(new Property(token.getCompiledAccessor()));
+            }
+
+        }
+
+        return astStatement;
+    }
+
+
+    public String getName() {
+        return "AST";
+    }
+}
