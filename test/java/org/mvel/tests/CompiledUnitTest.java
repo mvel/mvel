f590bd35c51f845ec4c0e7f6e2d5acb8ba13a8d8
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2007-01-17 07:05:44 +0000

    Refactoring

--- test/java/org/mvel/tests/CompiledUnitTest.java
+++ test/java/org/mvel/tests/CompiledUnitTest.java
@@ -540,7 +540,7 @@ public class CompiledUnitTest extends TestCase {
         ep.setCompiledStatement(compileExpression(text));
         ep.setVariableResolverFactory(variableTable);
 
-        for (int i = 0; i < 100000; i++) {
+        for (int i = 0; i < 1000; i++) {
             ep.executeFast();
         }
     }
