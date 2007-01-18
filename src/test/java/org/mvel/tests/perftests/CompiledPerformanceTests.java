d6312b5e99f170a9d96a99790ee4a7406ba1481e
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2007-01-18 04:46:43 +0000

    Refactoring

---
+++ src/test/java/org/mvel/tests/perftests/CompiledPerformanceTests.java
@@ -0,0 +1,58 @@
+package org.mvel.tests.perftests;
+
+import junit.framework.TestCase;
+import org.mvel.ExpressionParser;
+import static org.mvel.ExpressionParser.compileExpression;
+import org.mvel.integration.impl.MapVariableResolverFactory;
+import org.mvel.tests.main.CompiledUnitTest;
+import org.mvel.tests.main.res.Bar;
+import org.mvel.tests.main.res.Base;
+import org.mvel.tests.main.res.Foo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CompiledPerformanceTests extends TestCase {
+
+    protected Foo foo = new Foo();
+    protected Map<String, Object> map = new HashMap<String, Object>();
+    protected Base base = new Base();
+
+    public CompiledPerformanceTests() {
+        foo.setBar(new Bar());
+        map.put("foo", foo);
+        map.put("a", null);
+        map.put("b", null);
+        map.put("c", "cat");
+        map.put("BWAH", "");
+
+        map.put("misc", new CompiledUnitTest.MiscTestClass());
+
+        map.put("pi", "3.14");
+        map.put("hour", "60");
+        map.put("zero", 0);
+    }
+
+    public void testToListBenchmark() {
+        String text = "misc.toList(foo.bar.name, 'hello', 42, ['key1' : 'value1', c : [ foo.bar.age, 'car', 42 ]], [42, [c : 'value1']] )";
+
+        MapVariableResolverFactory variableTable = new MapVariableResolverFactory(map);
+        variableTable.pack();
+
+        ExpressionParser ep = new ExpressionParser();
+        ep.setCompiledStatement(compileExpression(text));
+        ep.setVariableResolverFactory(variableTable);
+
+        for (int i = 0; i < 1000000; i++) {
+            ep.executeFast();
+        }
+    }
+
+    public void testToListBenchmark2() {
+        testToListBenchmark();
+    }
+
+    public void testToListBenchmark3() {
+        testToListBenchmark();
+    }
+}
