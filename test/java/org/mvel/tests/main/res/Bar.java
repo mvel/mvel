679e3222dedebb43dea4d830a40b70e793314c27
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2007-01-18 04:44:38 +0000

    Refactoring

---
+++ test/java/org/mvel/tests/main/res/Bar.java
@@ -0,0 +1,36 @@
+package org.mvel.tests.main.res;
+
+public class Bar {
+    private String name = "dog";
+    private boolean woof = true;
+    private int age = 14;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isWoof() {
+        return woof;
+    }
+
+    public void setWoof(boolean woof) {
+        this.woof = woof;
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isFoo(Object obj) {
+        return obj instanceof Foo;
+    }
+}
