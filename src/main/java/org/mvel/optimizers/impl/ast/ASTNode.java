c248b546f5b8dbbce6aadb55d8670e35ce4c12b6
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2007-01-28 04:14:04 +0000

    the AST thing is a dead horse.  it's been superceded by the new reflective optimizer.

--- src/main/java/org/mvel/optimizers/impl/ast/ASTNode.java
+++
@@ -1,9 +0,0 @@
-package org.mvel.optimizers.impl.ast;
-
-import org.mvel.integration.VariableResolverFactory;
-
-public interface ASTNode {
-    public Object getValue(Object baseLineContext, Object staticContext, VariableResolverFactory vFactory);
-
-    public ASTNode setNextNode(ASTNode nextNode);
-}
