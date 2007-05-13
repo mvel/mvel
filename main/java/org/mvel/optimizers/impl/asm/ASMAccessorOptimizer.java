963ad35e3d8248e2b20a7cd99f74410b38cd21ee
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2007-05-13 20:44:29 +0000

    de-generified some entrant methods.

--- main/java/org/mvel/optimizers/impl/asm/ASMAccessorOptimizer.java
+++ main/java/org/mvel/optimizers/impl/asm/ASMAccessorOptimizer.java
@@ -721,13 +721,11 @@ public class ASMAccessorOptimizer extends AbstractOptimizer implements AccessorO
     }
 
 
-    private static final ClassLoader classLoader;
     private static final Method defineClass;
 
     static {
         try {
-            classLoader = ASMAccessorOptimizer.class.getClassLoader();
-            defineClass = classLoader.getClass().getDeclaredMethod("defineClass",
+            defineClass = ASMAccessorOptimizer.class.getClassLoader().getClass().getDeclaredMethod("defineClass",
                     new Class[]{String.class, byte[].class, int.class, int.class});
         }
         catch (Exception e) {
