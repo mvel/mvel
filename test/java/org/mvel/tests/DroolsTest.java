2927a78a03c8e060aa2e158d8d1cb6f3472927c9
mproctor <mproctor@9462aea2-c925-0410-9681-c018ebb51820>
2007-05-27 03:07:25 +0000

    -temp test

--- test/java/org/mvel/tests/DroolsTest.java
+++ test/java/org/mvel/tests/DroolsTest.java
@@ -1,12 +1,30 @@
 package org.mvel.tests;
 
 import java.util.HashMap;
+import java.util.Map;
+
+import junit.framework.TestCase;
 
 import org.mvel.MVEL;
 
-public class DroolsTest {
+public class DroolsTest extends TestCase {
     public void test1() throws Exception {    
         MVEL.executeExpression( MVEL.compileExpression( "new Integer(5)" ),
                                 null, new HashMap() );  
     }
+    
+    
+    public void test2() throws Exception {    
+        String str = (String) MVEL.executeExpression( MVEL.compileExpression( "\"hello2\"" ),
+                                null,
+                                new HashMap());
+        System.out.println( str );
+    }    
+    
+    public void test3() throws Exception {    
+        String str = (String) MVEL.executeExpression( MVEL.compileExpression( "new String(\"hello2\")" ),
+                                null,
+                                new HashMap());
+        System.out.println( str );
+    }        
 }
