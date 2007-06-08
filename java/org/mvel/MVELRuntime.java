4adaf8429221b18ae27446537df031cd63682d32
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2007-06-08 06:23:04 +0000

    performance improvements

--- java/org/mvel/MVELRuntime.java
+++ java/org/mvel/MVELRuntime.java
@@ -19,9 +19,7 @@ import java.util.Set;
 import static java.util.regex.Pattern.compile;
 
 public class MVELRuntime {
-
     private static ThreadLocal<Set<Integer>> breakpoints;
-    private boolean debugger = false;
 
     /**
      * Main interpreter loop.
@@ -32,14 +30,12 @@ public class MVELRuntime {
      */
     public static Object execute(boolean debugger, FastASTIterator tokens, Object ctx, VariableResolverFactory variableFactory) {
         Stack stk = new ExecutionStack();
-
         ASTNode tk = null;
         Integer operator;
 
         try {
             while ((tk = tokens.nextToken()) != null) {
                 if (tk.fields == -1) {
-
                     if (breakpoints != null && breakpoints.get() != null) {
                         debugger = true;
                     }
@@ -97,7 +93,6 @@ public class MVELRuntime {
                             stk.discard();
                             continue;
                         }
-
                     case TERNARY:
                         if (!(Boolean) stk.pop()) {
                             while (tokens.hasMoreTokens() && !tokens.nextToken().isOperator(Operator.TERNARY_ELSE)) ;
@@ -105,10 +100,8 @@ public class MVELRuntime {
                         stk.clear();
                         continue;
 
-
                     case TERNARY_ELSE:
                         return stk.pop();
-
 
                     case END_OF_STMT:
                         /**
@@ -231,7 +224,6 @@ public class MVELRuntime {
                 }
                 catch (ClassCastException e) {
                     throw new CompileException("syntax error or incomptable types", e);
-
                 }
                 catch (Exception e) {
                     throw new CompileException("failed to subEval expression", e);
@@ -239,7 +231,6 @@ public class MVELRuntime {
             }
 
             return stk.peek();
-
         }
         catch (NullPointerException e) {
             if (tk != null && tk.isOperator() && !tokens.hasMoreTokens()) {
@@ -252,161 +243,11 @@ public class MVELRuntime {
         }
     }
 
-//    /**
-//     * This method is called to subEval a binary statement (or junction).  The difference between a binary and
-//     * trinary statement, as far as the parser is concerned is that a binary statement has an entrant state,
-//     * where-as a trinary statement does not.  Consider: (x && y): in this case, x will be reduced first, and
-//     * therefore will have a value on the stack, so the parser will then process the next statement as a binary,
-//     * which is (&& y).
-//     * <p/>
-//     * You can also think of a binary statement in terms of: ({stackvalue} op value)
-//     *
-//     * @param o - operator
-//     * @return int - behaviour code
-//     */
-//    private static int reduceBinary(Stack stk, int o) {
-//        // assert debug("BINARY_OP " + o + " PEEK=<<" + stk.peek() + ">>");
-//    }
-//
-//    /**
-//     * This method is called when we reach the point where we must subEval a trinary operation in the expression.
-//     * (ie. val1 op val2).  This is not the same as a binary operation, although binary operations would appear
-//     * to have 3 structures as well.  A binary structure (or also a junction in the expression) compares the
-//     * current state against 2 downrange structures (usually an op and a val).
-//     */
-//    private void reduceTrinary() {
-//        Object v1, v2;
-//        Integer operator;
-//        try {
-//            while (stk.size() > 1) {
-//                operator = (Integer) stk.pop();
-//                v1 = stk.pop();
-//                v2 = stk.pop();
-//
-//                // assert debug("DO_TRINARY <<OPCODE_" + operator + ">> register1=" + v1 + "; register2=" + v2);
-//                switch (operator) {
-//                    case ADD:
-//                    case SUB:
-//                    case DIV:
-//                    case MULT:
-//                    case MOD:
-//                    case EQUAL:
-//                    case NEQUAL:
-//                    case GTHAN:
-//                    case LTHAN:
-//                    case GETHAN:
-//                    case LETHAN:
-//                    case POWER:
-//                        stk.push(doOperations(v2, operator, v1));
-//                        break;
-//
-//                    case CHOR:
-//                        if (!isEmpty(v2) || !isEmpty(v1)) {
-//                            stk.clear();
-//                            stk.push(!isEmpty(v2) ? v2 : v1);
-//                            return;
-//                        }
-//                        else stk.push(null);
-//                        break;
-//
-//                    case REGEX:
-//                        stk.push(compile(valueOf(v1)).matcher(valueOf(v2)).matches());
-//                        break;
-//
-//                    case INSTANCEOF:
-//                        if (v1 instanceof Class)
-//                            stk.push(((Class) v1).isInstance(v2));
-//                        else
-//                            stk.push(forName(valueOf(v1)).isInstance(v2));
-//
-//                        break;
-//
-//                    case CONVERTABLE_TO:
-//                        if (v1 instanceof Class)
-//                            stk.push(canConvert(v2.getClass(), (Class) v1));
-//                        else
-//                            stk.push(canConvert(v2.getClass(), forName(valueOf(v1))));
-//                        break;
-//
-//                    case CONTAINS:
-//                        stk.push(containsCheck(v2, v1));
-//                        break;
-//
-//                    case BW_AND:
-//                        stk.push(asInt(v2) & asInt(v1));
-//                        break;
-//
-//                    case BW_OR:
-//                        stk.push(asInt(v2) | asInt(v1));
-//                        break;
-//
-//                    case BW_XOR:
-//                        stk.push(asInt(v2) ^ asInt(v1));
-//                        break;
-//
-//                    case BW_SHIFT_LEFT:
-//                        stk.push(asInt(v2) << asInt(v1));
-//                        break;
-//
-//                    case BW_USHIFT_LEFT:
-//                        int iv2 = asInt(v2);
-//                        if (iv2 < 0) iv2 *= -1;
-//                        stk.push(iv2 << asInt(v1));
-//                        break;
-//
-//                    case BW_SHIFT_RIGHT:
-//                        stk.push(asInt(v2) >> asInt(v1));
-//                        break;
-//
-//                    case BW_USHIFT_RIGHT:
-//                        stk.push(asInt(v2) >>> asInt(v1));
-//                        break;
-//
-//                    case STR_APPEND:
-//                        stk.push(new StringAppender(valueOf(v2)).append(valueOf(v1)).toString());
-//                        break;
-//
-//                    case SOUNDEX:
-//                        stk.push(Soundex.soundex(valueOf(v1)).equals(Soundex.soundex(valueOf(v2))));
-//                        break;
-//
-//                    case SIMILARITY:
-//                        stk.push(similarity(valueOf(v1), valueOf(v2)));
-//                        break;
-//                }
-//            }
-//        }
-//        catch (ClassCastException e) {
-//            throw new CompileException("syntax error or incomptable types", e);
-//
-//        }
-//        catch (Exception e) {
-//            throw new CompileException("failed to subEval expression", e);
-//        }
-//    }
-
-    //
     private static int asInt(final Object o) {
         return (Integer) o;
     }
-//
-//
-//    private boolean hasNoMore() {
-//        return !tokens.hasMoreTokens();
-//    }
-//
-//    private boolean unwindStatement() {
-//        //noinspection StatementWithEmptyBody
-//        while (tokens.hasMoreTokens() && !tokens.nextToken().isOperator(Operator.END_OF_STMT)) ;
-//        return !tokens.hasMoreTokens();
-//    }
-//
-//    @SuppressWarnings({"StatementWithEmptyBody"})
-//    private void skipToOperator(int operator) {
-//        while (tokens.hasMoreTokens() && !tokens.nextToken().isOperator(operator)) ;
-//    }
 
-    public void registerBreakpoint(int line) {
+    public static void registerBreakpoint(int line) {
         if (breakpoints == null) {
             breakpoints = new ThreadLocal<Set<Integer>>();
             breakpoints.set(new HashSet<Integer>());
@@ -414,23 +255,15 @@ public class MVELRuntime {
         breakpoints.get().add(line);
     }
 
-    public void removeBreakpoint(int line) {
+    public static void removeBreakpoint(int line) {
         if (breakpoints != null && breakpoints.get() != null) {
             breakpoints.get().remove(line);
         }
     }
 
-    public void clearAllBreakpoints() {
+    public static void clearAllBreakpoints() {
         if (breakpoints != null && breakpoints.get() != null) {
             breakpoints.get().clear();
         }
-    }
-
-    public boolean isDebugger() {
-        return debugger;
-    }
-
-    public void setDebugger(boolean debugger) {
-        this.debugger = debugger;
     }
 }
