407239c73fed6e12a4316fb945ff2999b6daad7d
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2007-06-24 03:12:58 +0000

    fix

--- src/main/java/org/mvel/debug/Frame.java
+++ src/main/java/org/mvel/debug/Frame.java
@@ -4,4 +4,25 @@ public class Frame {
     private String sourceName;
     private int lineNumber;
 
+    public Frame(String sourceName, int lineNumber) {
+        this.sourceName = sourceName;
+        this.lineNumber = lineNumber;
+    }
+
+
+    public String getSourceName() {
+        return sourceName;
+    }
+
+    public void setSourceName(String sourceName) {
+        this.sourceName = sourceName;
+    }
+
+    public int getLineNumber() {
+        return lineNumber;
+    }
+
+    public void setLineNumber(int lineNumber) {
+        this.lineNumber = lineNumber;
+    }
 }
