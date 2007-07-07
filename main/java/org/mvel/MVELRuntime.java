3e339cacb8e662b861707d571a9cf3622b2707ce
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2007-07-07 17:57:33 +0000

    compiler and runtime performance improvements.

--- main/java/org/mvel/MVELRuntime.java
+++ main/java/org/mvel/MVELRuntime.java
@@ -19,7 +19,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import static java.util.regex.Pattern.compile;
 
 public class MVELRuntime {
     private static ThreadLocal<Map<String, Set<Integer>>> threadBreakpoints;
