523176518197a3bae15a157570faf624563e00b4
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2007-07-07 18:16:18 +0000

    JIT performance improvements.

--- src/test/java/org/mvel/tests/AccessorBMModel.java
+++ src/test/java/org/mvel/tests/AccessorBMModel.java
@@ -3,18 +3,13 @@ package org.mvel.tests;
 import org.mvel.Accessor;
 import org.mvel.ExecutableStatement;
 import org.mvel.integration.VariableResolverFactory;
+import org.mvel.tests.main.res.Foo;
 
 
 public class AccessorBMModel implements Accessor {
     private ExecutableStatement p0;
 
     public Object getValue(Object ctx, Object elCtx, VariableResolverFactory variableFactory) {
-        // return Math.sqrt(DataConversion.convert(p0.getValue(ctx, variableFactory), Double.class).doubleValue());
-
-        return Integer.class;
-
-        //return ((CharSequence) ctx).charAt(DataConversion.convert(p0.getValue(ctx, variableFactory), Integer.class));
-
-        // return DataConversion.convert(new Integer(5), Integer.class);
+        return ((Foo) ctx).getBar().getName();
     }
 }
