46023889b41a763a6f306e8f5dba5dc1bf931c82
mproctor <mproctor@9462aea2-c925-0410-9681-c018ebb51820>
2007-07-10 23:03:40 +0000

    made a copy

---
+++ 1.2pre2/src/main/java/org/mvel/integration/impl/ClassImportResolverFactory.java
@@ -0,0 +1,80 @@
+/**
+ * MVEL (The MVFLEX Expression Language)
+ *
+ * Copyright (C) 2007 Christopher Brock, MVFLEX/Valhalla Project and the Codehaus
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.mvel.integration.impl;
+
+import org.mvel.integration.VariableResolver;
+import static org.mvel.util.ParseTools.getSimpleClassName;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ClassImportResolverFactory extends BaseVariableResolverFactory {
+    public ClassImportResolverFactory() {
+        super();
+
+        variableResolvers = new HashMap<String, VariableResolver>();
+    }
+
+    public VariableResolver createVariable(String name, Object value) {
+        throw new RuntimeException("unimplemented, please use addClass(Class)");
+
+//        VariableResolver vr = new ClassImportResolver(name.substring(name.lastIndexOf('.')), name);
+//        variableResolvers.put(vr.getName(), vr);
+//        return vr;
+    }
+
+
+    public VariableResolver createVariable(String name, Object value, Class type) {
+        throw new RuntimeException("attempt to created a typed import type.  this is highly redundant and more " +
+                "importantly, not implemented.");
+    }
+
+    public void addClass(Class clazz) {
+        variableResolvers.put(getSimpleClassName(clazz), new ClassImportResolver(getSimpleClassName(clazz), clazz));
+    }
+
+    public boolean isTarget(String name) {
+        return variableResolvers.containsKey(name);
+    }
+
+    public boolean isResolveable(String name) {
+        return variableResolvers.containsKey(name) || isNextResolveable(name);
+    }
+
+    public void clear() {
+        variableResolvers.clear();
+    }
+
+    public void setImportedClasses(Map<String, Class> imports) {
+        if (imports == null) return;
+        for (String var : imports.keySet()) {
+            variableResolvers.put(var, new ClassImportResolver(var, imports.get(var)));
+        }
+    }
+
+    public Map<String, Object> getImportedClasses() {
+        Map<String, Object> imports = new HashMap<String, Object>();
+        for (String var : variableResolvers.keySet()) {
+            imports.put(var, (Class) variableResolvers.get(var).getValue());
+        }
+
+        return imports;
+    }
+
+}
---
+++ 1.2pre2/src/test/java/org/mvel/tests/DroolsTest.java
@@ -0,0 +1,198 @@
+package org.mvel.tests;
+
+import junit.framework.TestCase;
+
+public class DroolsTest extends TestCase {
+//
+//    private static ClassLoader classLoader;
+//    private static Method defineClass;
+//
+//    public final static byte[] getRawBytes(File f) {
+//        try {
+//            if (!f.exists()) return null;
+//            FileInputStream fin = new FileInputStream(f);
+//            byte[] buffer = new byte[(int) f.length()];
+//            fin.read(buffer);
+//            fin.close();
+//            return buffer;
+//        }
+//        catch (Exception err) {
+//            err.printStackTrace();
+//            return null;
+//        }
+//    }
+//
+//    private java.lang.Class loadClass(String className,
+//                                      byte[] b) throws Exception {
+//        synchronized (defineClass) {
+//            defineClass.setAccessible(true);
+//            try {
+//                //noinspection RedundantArrayCreation
+//                return (Class) defineClass.invoke(classLoader,
+//                        new Object[]{className, b, 0, (b.length)});
+//            }
+//            catch (Exception t) {
+//                //dumpAdvancedDebugging();
+//                throw t;
+//            }
+//            finally {
+//                defineClass.setAccessible(false);
+//            }
+//        }
+//    }
+//
+//    public void test1() throws Exception {
+//        MVEL.executeExpression(MVEL.compileExpression("new Integer(5)"),
+//                null, new HashMap());
+//    }
+//
+//    public void test2() throws Exception {
+//        byte[] bytes = ASMIntegerDump1.dump();
+//
+//        classLoader = Thread.currentThread().getContextClassLoader();
+//        defineClass = ClassLoader.class.getDeclaredMethod("defineClass",
+//                new Class[]{String.class, byte[].class, int.class, int.class});
+//        Class clazz = loadClass("org.mvel.tests.ASMInteger1",
+//                bytes);
+//
+//    }
+//
+//    public void test3() throws Exception {
+//        byte[] bytes = ASMIntegerDump2.dump();
+//
+//        classLoader = Thread.currentThread().getContextClassLoader();
+//        defineClass = ClassLoader.class.getDeclaredMethod("defineClass",
+//                new Class[]{String.class, byte[].class, int.class, int.class});
+//        Class clazz = loadClass("org.mvel.tests.ASMInteger2",
+//                bytes);
+//
+//    }
+//
+//    public static class ASMIntegerDump1
+//            implements
+//            Opcodes {
+//
+//
+//        public static byte[] dump() throws Exception {
+//
+//            ClassWriter cw = new ClassWriter(0);
+//            FieldVisitor fv;
+//            MethodVisitor mv;
+//            AnnotationVisitor av0;
+//
+//            cw.visit(V1_5, ACC_PUBLIC + ACC_SUPER, "org/mvel/tests/ASMInteger1", null, "java/lang/Object", new String[]{"org/mvel/Accessor"});
+//
+//            {
+//                fv = cw.visitField(ACC_PRIVATE, "p0", "Lorg/mvel/ExecutableStatement;", null, null);
+//                fv.visitEnd();
+//            }
+//            {
+//                mv = cw.visitMethod(ACC_PUBLIC, "<init>", "()V", null, null);
+//                mv.visitCode();
+//                mv.visitVarInsn(ALOAD, 0);
+//                mv.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V");
+//                mv.visitInsn(RETURN);
+//                mv.visitMaxs(1, 1);
+//                mv.visitEnd();
+//            }
+//            {
+//                mv = cw.visitMethod(ACC_PUBLIC, "getValue", "(Ljava/lang/Object;Ljava/lang/Object;Lorg/mvel/integration/VariableResolverFactory;)Ljava/lang/Object;", null, null);
+//                mv.visitCode();
+//                mv.visitTypeInsn(NEW, "java/lang/Integer");
+//                mv.visitInsn(DUP);
+//                mv.visitVarInsn(ALOAD, 0);
+//                mv.visitFieldInsn(GETFIELD, "org/mvel/tests/ASMInteger1", "p0", "Lorg/mvel/ExecutableStatement;");
+//                mv.visitVarInsn(ALOAD, 2);
+//                mv.visitVarInsn(ALOAD, 3);
+//                mv.visitMethodInsn(INVOKEINTERFACE, "org/mvel/ExecutableStatement", "getValue", "(Ljava/lang/Object;Lorg/mvel/integration/VariableResolverFactory;)Ljava/lang/Object;");
+//                mv.visitLdcInsn(Type.getType("Ljava/lang/Integer;"));
+//                mv.visitMethodInsn(INVOKESTATIC, "org/mvel/DataConversion", "convert", "(Ljava/lang/Object;Ljava/lang/Class;)Ljava/lang/Object;");
+//                mv.visitTypeInsn(CHECKCAST, "java/lang/Integer");
+//                mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/Integer", "intValue", "()I");
+//                mv.visitMethodInsn(INVOKESPECIAL, "Ljava/lang/Integer;", "<init>", "(I)V");
+//                mv.visitInsn(ARETURN);
+//                mv.visitMaxs(5, 4);
+//                mv.visitEnd();
+//            }
+//            {
+//                mv = cw.visitMethod(ACC_PUBLIC, "<init>", "(Lorg/mvel/ExecutableStatement;)V", null, null);
+//                mv.visitCode();
+//                mv.visitVarInsn(ALOAD, 0);
+//                mv.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V");
+//                mv.visitVarInsn(ALOAD, 0);
+//                mv.visitVarInsn(ALOAD, 1);
+//                mv.visitFieldInsn(PUTFIELD, "org/mvel/tests/ASMInteger1", "p0", "Lorg/mvel/ExecutableStatement;");
+//                mv.visitInsn(RETURN);
+//                mv.visitMaxs(2, 2);
+//                mv.visitEnd();
+//            }
+//            cw.visitEnd();
+//
+//            return cw.toByteArray();
+//        }
+//    }
+//
+//    public static class ASMIntegerDump2
+//            implements
+//            Opcodes {
+//
+//
+//        public static byte[] dump() throws Exception {
+//
+//            ClassWriter cw = new ClassWriter(0);
+//            FieldVisitor fv;
+//            MethodVisitor mv;
+//            AnnotationVisitor av0;
+//
+//            cw.visit(V1_5, ACC_PUBLIC + ACC_SUPER, "org/mvel/tests/ASMInteger2", null, "java/lang/Object", new String[]{"org/mvel/Accessor"});
+//
+//            {
+//                fv = cw.visitField(ACC_PRIVATE, "p0", "Lorg/mvel/ExecutableStatement;", null, null);
+//                fv.visitEnd();
+//            }
+//            {
+//                mv = cw.visitMethod(ACC_PUBLIC, "<init>", "()V", null, null);
+//                mv.visitCode();
+//                mv.visitVarInsn(ALOAD, 0);
+//                mv.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V");
+//                mv.visitInsn(RETURN);
+//                mv.visitMaxs(1, 1);
+//                mv.visitEnd();
+//            }
+//            {
+//                mv = cw.visitMethod(ACC_PUBLIC, "getValue", "(Ljava/lang/Object;Ljava/lang/Object;Lorg/mvel/integration/VariableResolverFactory;)Ljava/lang/Object;", null, null);
+//                mv.visitCode();
+//                mv.visitTypeInsn(NEW, "java/lang/Integer");
+//                mv.visitInsn(DUP);
+//                mv.visitVarInsn(ALOAD, 0);
+//                mv.visitFieldInsn(GETFIELD, "org/mvel/tests/ASMInteger2", "p0", "Lorg/mvel/ExecutableStatement;");
+//                mv.visitVarInsn(ALOAD, 2);
+//                mv.visitVarInsn(ALOAD, 3);
+//                mv.visitMethodInsn(INVOKEINTERFACE, "org/mvel/ExecutableStatement", "getValue", "(Ljava/lang/Object;Lorg/mvel/integration/VariableResolverFactory;)Ljava/lang/Object;");
+//                mv.visitLdcInsn(Type.getType("Ljava/lang/Integer;"));
+//                mv.visitMethodInsn(INVOKESTATIC, "org/mvel/DataConversion", "convert", "(Ljava/lang/Object;Ljava/lang/Class;)Ljava/lang/Object;");
+//                mv.visitTypeInsn(CHECKCAST, "java/lang/Integer");
+//                mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/Integer", "intValue", "()I");
+//                mv.visitMethodInsn(INVOKESPECIAL, "java/lang/Integer", "<init>", "(I)V");
+//                mv.visitInsn(ARETURN);
+//                mv.visitMaxs(5, 4);
+//                mv.visitEnd();
+//            }
+//            {
+//                mv = cw.visitMethod(ACC_PUBLIC, "<init>", "(Lorg/mvel/ExecutableStatement;)V", null, null);
+//                mv.visitCode();
+//                mv.visitVarInsn(ALOAD, 0);
+//                mv.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V");
+//                mv.visitVarInsn(ALOAD, 0);
+//                mv.visitVarInsn(ALOAD, 1);
+//                mv.visitFieldInsn(PUTFIELD, "org/mvel/tests/ASMInteger2", "p0", "Lorg/mvel/ExecutableStatement;");
+//                mv.visitInsn(RETURN);
+//                mv.visitMaxs(2, 2);
+//                mv.visitEnd();
+//            }
+//            cw.visitEnd();
+//
+//            return cw.toByteArray();
+//        }
+//    }
+}
