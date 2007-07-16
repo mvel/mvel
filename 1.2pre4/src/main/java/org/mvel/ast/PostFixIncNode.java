1b801f534e6c82073e963c53d1ec3243827947f3
mproctor <mproctor@9462aea2-c925-0410-9681-c018ebb51820>
2007-07-16 19:08:50 +0000

    made a copy

---
+++ 1.2pre4/src/main/java/org/mvel/ast/PostFixIncNode.java
@@ -0,0 +1,30 @@
+package org.mvel.ast;
+
+import org.mvel.ASTNode;
+import org.mvel.Operator;
+import org.mvel.integration.VariableResolver;
+import org.mvel.integration.VariableResolverFactory;
+import static org.mvel.util.ParseTools.doOperations;
+
+/**
+ * @author Christopher Brock
+ */
+public class PostFixIncNode extends ASTNode {
+    private String name;
+
+    public PostFixIncNode(char[] expr, int fields) {
+        super(expr, fields);
+        name = new String(expr);
+    }
+
+    public Object getReducedValueAccelerated(Object ctx, Object thisValue, VariableResolverFactory factory) {
+        VariableResolver vResolver = factory.getVariableResolver(name);
+        Object o = vResolver.getValue();
+        vResolver.setValue(doOperations(o, Operator.ADD, 1));
+        return o;
+    }
+
+    public Object getReducedValue(Object ctx, Object thisValue, VariableResolverFactory factory) {
+        return getReducedValueAccelerated(ctx, thisValue, factory);
+    }
+}
