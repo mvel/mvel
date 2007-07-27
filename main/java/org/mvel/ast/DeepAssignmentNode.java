03c19ce89b24f29673d408d7768a6d6ae63279d6
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2007-07-27 21:47:29 +0000

    higher performance deep assignments and overloaded assignments.

--- main/java/org/mvel/ast/DeepAssignmentNode.java
+++ main/java/org/mvel/ast/DeepAssignmentNode.java
@@ -1,9 +1,11 @@
 package org.mvel.ast;
 
+import static org.mvel.PropertyAccessor.set;
+import static org.mvel.MVEL.eval;
 import org.mvel.*;
+import static org.mvel.MVEL.compileSetExpression;
 import org.mvel.integration.VariableResolverFactory;
-import org.mvel.util.ParseTools;
-import static org.mvel.util.ArrayTools.findFirst;
+import static org.mvel.util.ParseTools.subCompileExpression;
 import static org.mvel.util.ParseTools.subset;
 import static org.mvel.util.PropertyTools.find;
 
@@ -11,9 +13,8 @@ import static org.mvel.util.PropertyTools.find;
  * @author Christopher Brock
  */
 public class DeepAssignmentNode extends ASTNode implements Assignment {
-     private String property;
-
-  //  private Accessor baseAccessor;
+    private String property;
+    private char[] stmt;
 
     private CompiledSetExpression set;
     private Accessor statement;
@@ -21,43 +22,31 @@ public class DeepAssignmentNode extends ASTNode implements Assignment {
     public DeepAssignmentNode(char[] expr, int fields) {
         super(expr, fields);
 
-       String name;
-
         int mark;
         if ((mark = find(expr, '=')) != -1) {
-            name = new String(expr, 0, mark).trim();
-            statement = (ExecutableStatement) ParseTools.subCompileExpression(subset(expr, mark + 1));
-                       
+            property = new String(expr, 0, mark).trim();
+            statement = (ExecutableStatement) subCompileExpression(stmt = subset(expr, mark + 1));
         }
         else {
-            name = new String(expr);
+            property = new String(expr);
         }
 
-//        baseAccessor = (Accessor) ParseTools.subCompileExpression(name.substring(0, mark = name.indexOf('.')));
-        property = name;
-        
-        set = (CompiledSetExpression) MVEL.compileSetExpression(property.toCharArray());
-
-
-
-
+        if ((fields & COMPILE_IMMEDIATE) != 0) {
+            set = (CompiledSetExpression) compileSetExpression(property.toCharArray());
+        }
     }
-
 
     public Object getReducedValueAccelerated(Object ctx, Object thisValue, VariableResolverFactory factory) {
         Object val;
-//        MVEL.setProperty(baseAccessor.getValue(ctx, thisValue, factory), property,
-//                val = statement.getValue(ctx, thisValue, factory));
-
         set.setValue(ctx, factory, val = statement.getValue(ctx, thisValue, factory));
-
         return val;
     }
 
     public Object getReducedValue(Object ctx, Object thisValue, VariableResolverFactory factory) {
-        return getReducedValueAccelerated(ctx, thisValue, factory);
+        Object val;
+        set(ctx, property, val = eval(stmt, ctx, factory));
+        return val;
     }
-
 
     public String getAssignmentVar() {
         return property;
