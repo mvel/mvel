b3038d2f1d5d0969d1bbe099e30a7ac022ab1f9b
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2007-07-27 22:33:58 +0000

    fixes to dynamic typing stuff with collection/array assignments

--- main/java/org/mvel/optimizers/impl/refl/ArrayAccessorNest.java
+++ main/java/org/mvel/optimizers/impl/refl/ArrayAccessorNest.java
@@ -1,12 +1,21 @@
 package org.mvel.optimizers.impl.refl;
 
 import org.mvel.AccessorNode;
+import static org.mvel.DataConversion.convert;
 import org.mvel.ExecutableStatement;
 import org.mvel.integration.VariableResolverFactory;
+import org.mvel.util.ParseTools;
 
 public class ArrayAccessorNest implements AccessorNode {
     private AccessorNode nextNode;
     private ExecutableStatement index;
+
+    public ArrayAccessorNest() {
+    }
+
+    public ArrayAccessorNest(String index) {
+        this.index = (ExecutableStatement) ParseTools.subCompileExpression(index);
+    }
 
     public Object getValue(Object ctx, Object elCtx, VariableResolverFactory vars) {
         if (nextNode != null) {
@@ -19,7 +28,7 @@ public class ArrayAccessorNest implements AccessorNode {
 
 
     public Object setValue(Object ctx, Object elCtx, VariableResolverFactory variableFactory, Object value) {
-       return ((Object[]) ctx)[(Integer) index.getValue(ctx, elCtx, variableFactory)] = value; 
+       return ((Object[]) ctx)[(Integer) index.getValue(ctx, elCtx, variableFactory)] = convert(value, ctx.getClass().getComponentType()); 
     }
 
     public ExecutableStatement getIndex() {
