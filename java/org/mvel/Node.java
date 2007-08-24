2cf292eb22b97059ccec1c07ae6daaac61f4dee1
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2007-08-24 22:21:18 +0000

    minor fix.

--- java/org/mvel/Node.java
+++ java/org/mvel/Node.java
@@ -155,7 +155,15 @@ public class Node implements Cloneable {
         n.endNode = endNode;
         n.alias = alias;
         n.name = name;
-        n.register = register;
+
+        if (register != null) {
+            if (register instanceof ForeachContext) {
+                n.register = ((ForeachContext) register).clone();
+            }
+            else {
+                n.register = register;
+            }
+        }
 
         return n;
     }
