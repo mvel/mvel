36afd8b250bf6be8c1bfd5d91effe0312491c5c2
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2007-08-28 23:41:20 +0000

    potential fix #2

--- java/org/mvel/ast/NewObjectNode.java
+++ java/org/mvel/ast/NewObjectNode.java
@@ -4,6 +4,7 @@ import org.mvel.ASTNode;
 import static org.mvel.AbstractParser.getCurrentThreadParserContext;
 import org.mvel.Accessor;
 import org.mvel.ParserContext;
+import org.mvel.CompileException;
 import org.mvel.integration.VariableResolverFactory;
 import org.mvel.optimizers.AccessorOptimizer;
 import static org.mvel.optimizers.OptimizerFactory.getThreadAccessorOptimizer;
@@ -16,27 +17,27 @@ import static java.lang.Class.forName;
  */
 public class NewObjectNode extends ASTNode {
     private transient Accessor newObjectOptimizer;
+    private String className;
 
     public NewObjectNode(char[] expr, int fields) {
         super(expr, fields);
 
         if ((fields & COMPILE_IMMEDIATE) != 0) {
             int endRange = findFirst('(', expr);
-            String name;
             if (endRange == -1) {
-                name = new String(expr);
+                className = new String(expr);
             }
             else {
-                name = new String(expr, 0, findFirst('(', expr));
+                className = new String(expr, 0, findFirst('(', expr));
             }
 
             ParserContext pCtx = getCurrentThreadParserContext();
-            if (pCtx != null && pCtx.hasImport(name)) {
-                egressType = pCtx.getImport(name);
+            if (pCtx != null && pCtx.hasImport(className)) {
+                egressType = pCtx.getImport(className);
             }
             else {
                 try {
-                    egressType = forName(name);
+                    egressType = forName(className);
                 }
                 catch (ClassNotFoundException e) {
                     //           throw new CompileException("class not found: " + name, e);
@@ -45,24 +46,46 @@ public class NewObjectNode extends ASTNode {
 
 
             if (egressType != null) {
-                String FQCN = egressType.getName();
-
-                if (!name.equals(FQCN)) {
-                    int idx = FQCN.lastIndexOf('$');
-                    if (idx != -1 && name.lastIndexOf('$') == -1) {
-                        this.name = (FQCN.substring(0, idx + 1) + new String(this.name)).toCharArray();
-                    }
-                    else {
-                        this.name = (FQCN.substring(0, FQCN.lastIndexOf('.') + 1) + new String(this.name)).toCharArray();
-                    }
-                }
+                rewriteClassReferenceToFQCN();
             }
         }
 
     }
 
+    private void rewriteClassReferenceToFQCN() {
+        String FQCN = egressType.getName();
+
+        if (!className.equals(FQCN)) {
+            int idx = FQCN.lastIndexOf('$');
+            if (idx != -1 && className.lastIndexOf('$') == -1) {
+                this.name = (FQCN.substring(0, idx + 1) + new String(this.name)).toCharArray();
+            }
+            else {
+                this.name = (FQCN.substring(0, FQCN.lastIndexOf('.') + 1) + new String(this.name)).toCharArray();
+            }
+        }
+    }
+
     public Object getReducedValueAccelerated(Object ctx, Object thisValue, VariableResolverFactory factory) {
         if (newObjectOptimizer == null) {
+            if (egressType == null) {
+                /**
+                 * This means we couldn't resolve the type at the time this AST node was created, which means
+                 * we have to attempt runtime resolution.
+                 */
+
+                if (factory != null && factory.isResolveable(className)) {
+                    try {
+                        egressType = (Class) factory.getVariableResolver(className).getValue();
+                        rewriteClassReferenceToFQCN();
+                    }
+                    catch (ClassCastException e) {
+                        throw new CompileException("cannot construct object: " + className + " is not a class reference", e);
+                    }
+                }
+            }
+
+
             AccessorOptimizer optimizer = getThreadAccessorOptimizer();
             newObjectOptimizer = optimizer.optimizeObjectCreation(name, ctx, thisValue, factory);
 
