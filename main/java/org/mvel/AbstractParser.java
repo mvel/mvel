0aa6f2e450dc1aefbecc51dacdaa50b2ce0867da
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2007-08-28 21:00:58 +0000

    removal of redundant construction of object during optimization of new object.

--- main/java/org/mvel/AbstractParser.java
+++ main/java/org/mvel/AbstractParser.java
@@ -210,7 +210,7 @@ public class AbstractParser implements Serializable {
                 lastWasComment = lastWasLineLabel = false;
             }
         }
-
+                                                                              
         /**
          * Skip any whitespace currently under the starting point.
          */
