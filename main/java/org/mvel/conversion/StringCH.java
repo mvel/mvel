913bd05d34a60f38fb4f122cda8a3a7705dd6e8d
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2007-08-29 19:57:14 +0000

    :(

--- main/java/org/mvel/conversion/StringCH.java
+++ main/java/org/mvel/conversion/StringCH.java
@@ -2,9 +2,11 @@ package org.mvel.conversion;
 
 import org.mvel.ConversionHandler;
 
+import static java.lang.String.valueOf;
+
 public class StringCH implements ConversionHandler {
     public Object convertFrom(Object in) {
-        return String.valueOf(in);
+        return valueOf(in);
     }
 
 
