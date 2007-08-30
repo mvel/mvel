4ac833c7aa7fe417c9a98224b500ca7007a2d415
mproctor <mproctor@9462aea2-c925-0410-9681-c018ebb51820>
2007-08-30 14:06:48 +0000

    made a copy

---
+++ 1.2rc4rv908/src/main/java/org/mvel/ast/AssertNode.java
@@ -0,0 +1,34 @@
+package org.mvel.ast;
+
+import org.mvel.ASTNode;
+import org.mvel.CompileException;
+import org.mvel.ExecutableStatement;
+import org.mvel.integration.VariableResolverFactory;
+import org.mvel.util.ParseTools;
+
+/**
+ * @author Christopher Brock
+ */
+public class AssertNode extends ASTNode {
+    public ExecutableStatement assertion;
+
+    public AssertNode(char[] expr, int fields) {
+        super(expr, fields);
+        assertion = (ExecutableStatement) ParseTools.subCompileExpression(expr);
+    }
+
+    public Object getReducedValueAccelerated(Object ctx, Object thisValue, VariableResolverFactory factory) {
+        try {
+            Boolean bool = (Boolean) assertion.getValue(ctx, thisValue, factory);
+            if (!bool) throw new AssertionError("assertion failed in expression: " + new String(this.name));
+            return bool;
+        }
+        catch (ClassCastException e) {
+            throw new CompileException("assertion does not contain a boolean statement");
+        }
+    }
+
+    public Object getReducedValue(Object ctx, Object thisValue, VariableResolverFactory factory) {
+        return getReducedValueAccelerated(ctx, thisValue, factory);
+    }
+}
