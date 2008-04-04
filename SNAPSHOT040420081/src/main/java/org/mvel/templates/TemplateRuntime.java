db4f42b7746944666d275db02252af0d53f04c36
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2008-04-04 21:43:36 +0000

    [maven-release-plugin]  copy for tag SNAPSHOT040420081

---
+++ SNAPSHOT040420081/src/main/java/org/mvel/templates/TemplateRuntime.java
@@ -0,0 +1,109 @@
+package org.mvel.templates;
+
+import org.mvel.integration.VariableResolverFactory;
+import org.mvel.integration.impl.MapVariableResolverFactory;
+import static org.mvel.templates.TemplateCompiler.compileTemplate;
+import org.mvel.templates.res.Node;
+import org.mvel.templates.util.TemplateTools;
+import org.mvel.util.StringAppender;
+
+import java.util.Map;
+import java.io.InputStream;
+import java.io.File;
+
+public class TemplateRuntime {
+    private char[] template;
+    private TemplateRegistry namedTemplateRegistry;
+    private Node rootNode;
+
+    public TemplateRuntime(char[] template, TemplateRegistry namedTemplateRegistry, Node rootNode) {
+        this.template = template;
+        this.namedTemplateRegistry = namedTemplateRegistry;
+        this.rootNode = rootNode;
+    }
+
+    public static Object eval(File file, Object ctx, VariableResolverFactory vars, TemplateRegistry registry) {
+        return execute(compileTemplate(TemplateTools.readInFile(file)), ctx, vars, registry);
+    }
+
+    public static Object eval(InputStream instream, Object ctx, VariableResolverFactory vars, TemplateRegistry registry) {
+        return execute(compileTemplate(TemplateTools.readStream(instream)), ctx, vars, registry);
+    }
+
+    public static Object eval(String template, Map vars) {
+        return execute(compileTemplate(template), null, new MapVariableResolverFactory(vars));
+    }
+
+    public static Object eval(String template, Object ctx) {
+        return execute(compileTemplate(template), ctx, null);
+    }
+
+    public static Object eval(String template, Object ctx, Map vars) {
+        return execute(compileTemplate(template), ctx, new MapVariableResolverFactory(vars));
+    }
+
+    public static Object eval(String template, Object ctx, VariableResolverFactory vars) {
+        return execute(compileTemplate(template), ctx, vars);
+    }
+
+    public static Object eval(String template, Map vars, TemplateRegistry registry) {
+        return execute(compileTemplate(template), null, new MapVariableResolverFactory(vars), registry);
+    }
+
+    public static Object eval(String template, Object ctx, Map vars, TemplateRegistry registry) {
+        return execute(compileTemplate(template), ctx, new MapVariableResolverFactory(vars), registry);
+    }
+
+    public static Object eval(String template, Object ctx, VariableResolverFactory vars, TemplateRegistry registry) {
+        return execute(compileTemplate(template), ctx, vars, registry);
+    }
+
+
+    public static Object execute(CompiledTemplate compiled) {
+        return execute(compiled.getRoot(), compiled.getTemplate(), new StringAppender(), null, null, null);
+    }
+
+
+    public static Object execute(CompiledTemplate compiled, Object context, VariableResolverFactory factory) {
+        return execute(compiled.getRoot(), compiled.getTemplate(), new StringAppender(), context, factory, null);
+    }
+
+    public static Object execute(CompiledTemplate compiled, Object context, VariableResolverFactory factory, TemplateRegistry registry) {
+        return execute(compiled.getRoot(), compiled.getTemplate(), new StringAppender(), context, factory, registry);
+    }
+
+
+    public static Object execute(Node root, char[] template,
+                                 StringAppender appender, Object context,
+                                 VariableResolverFactory factory, TemplateRegistry registry) {
+        return new TemplateRuntime(template, registry, root).execute(appender, context, factory);
+    }
+
+    public Object execute(StringAppender appender, Object context, VariableResolverFactory factory) {
+        return rootNode.eval(this, appender, context, factory);
+    }
+
+    public Node getRootNode() {
+        return rootNode;
+    }
+
+    public void setRootNode(Node rootNode) {
+        this.rootNode = rootNode;
+    }
+
+    public char[] getTemplate() {
+        return template;
+    }
+
+    public void setTemplate(char[] template) {
+        this.template = template;
+    }
+
+    public TemplateRegistry getNamedTemplateRegistry() {
+        return namedTemplateRegistry;
+    }
+
+    public void setNamedTemplateRegistry(TemplateRegistry namedTemplateRegistry) {
+        this.namedTemplateRegistry = namedTemplateRegistry;
+    }
+}
