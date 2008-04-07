a9e902c3777870a7a9d035f831b1c4f767f85ffb
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2008-04-07 16:51:58 +0000

    copyright updated

--- src/main/java/org/mvel/sh/Main.java
+++ src/main/java/org/mvel/sh/Main.java
@@ -1,21 +1,17 @@
 package org.mvel.sh;
 
+import org.mvel.MVEL;
+
 public class Main {
-    public static final int VERSION = 1;
-    public static final int VERSION_SUB = 0;
-
-
-
     public static void main(String[] args) {
         showSplash();
         ShellSession session = new ShellSession();
         session.run();
     }
 
-
     private static void showSplash() {
         System.out.println("\nMVEL-Shell (MVELSH)");
-        System.out.println("Copyright (C) 2007, Christopher Brock, The Codehaus");
-        System.out.println("Version " + VERSION + "." + VERSION_SUB + "  -- Written by: Christopher Brock\n");
+        System.out.println("Copyright (C) 2008, Christopher Brock, The Codehaus");
+        System.out.println("Version " + MVEL.VERSION + "." + MVEL.VERSION_SUB + "  -- Written by: Christopher Brock\n");
     }
 }
