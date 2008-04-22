09099f5ea3f42787380e361c317a4c77a6cb3b6f
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2008-04-22 01:17:54 +0000

    1.3.0 Release Candidate 2

---
+++ 1.3.0RC1/1.3.0RC2/main/java/org/mvel/UnresolveablePropertyException.java
@@ -0,0 +1,22 @@
+package org.mvel;
+
+/**
+ * @author Christopher Brock
+ */
+public class UnresolveablePropertyException extends RuntimeException {
+    private ASTNode astNode;
+
+    public UnresolveablePropertyException(ASTNode astNode, Throwable throwable) {
+        super("unable to resolve token: " + astNode.getName(), throwable);
+        this.astNode = astNode;
+    }
+
+    public UnresolveablePropertyException(ASTNode astNode) {
+        super("unable to resolve token: " + astNode.getName());
+        this.astNode = astNode;
+    }
+
+    public ASTNode getToken() {
+        return astNode;
+    }
+}
