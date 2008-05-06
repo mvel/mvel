76b37af65fc329fa57ac120406c0c21ed92bb8ce
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2008-05-06 19:41:05 +0000

    fixed.

--- main/java/org/mvel/ParserContext.java
+++ main/java/org/mvel/ParserContext.java
@@ -3,9 +3,8 @@ package org.mvel;
 import org.mvel.ast.LineLabel;
 import org.mvel.integration.Interceptor;
 import org.mvel.util.MethodStub;
-import org.mvel.util.PropertyTools;
-import static org.mvel.util.PropertyTools.contains;
 import static org.mvel.util.ParseTools.getSimpleClassName;
+import static org.mvel.util.PropertyTools.contains;
 
 import java.io.Serializable;
 import static java.lang.Thread.currentThread;
