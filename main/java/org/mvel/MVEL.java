9a306ec81744c879c96f7b78dca698b7db5a63c2
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2008-05-09 19:50:07 +0000

    update version tag

--- main/java/org/mvel/MVEL.java
+++ main/java/org/mvel/MVEL.java
@@ -37,7 +37,7 @@ import java.util.Map;
 public class MVEL {
     public static final String NAME = "MVEL (MVFLEX Expression Language)";
     public static final String VERSION = "1.3";
-    public static final String VERSION_SUB = "0";
+    public static final String VERSION_SUB = "2";
     public static final String CODENAME = "horizon";
 
     static boolean DEBUG_FILE = getBoolean("mvel.debug.fileoutput");
