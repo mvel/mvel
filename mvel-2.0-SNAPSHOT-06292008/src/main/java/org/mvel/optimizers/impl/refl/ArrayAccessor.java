2d5a01ae48224d9decf5a778e0ece78f55bbc4ff
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2008-06-29 21:06:45 +0000

    [maven-release-plugin]  copy for tag mvel-2.0-SNAPSHOT-06292008

---
+++ mvel-2.0-SNAPSHOT-06292008/src/main/java/org/mvel/optimizers/impl/refl/ArrayAccessor.java
@@ -0,0 +1,73 @@
+/**
+ * MVEL (The MVFLEX Expression Language)
+ *
+ * Copyright (C) 2007 Christopher Brock, MVFLEX/Valhalla Project and the Codehaus
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.mvel.optimizers.impl.refl;
+
+import org.mvel.compiler.AccessorNode;
+import org.mvel.integration.VariableResolverFactory;
+
+import java.lang.reflect.Array;
+
+public class ArrayAccessor implements AccessorNode {
+    private AccessorNode nextNode;
+
+    private int index;
+
+    public ArrayAccessor() {
+    }
+
+    public ArrayAccessor(int index) {
+        this.index = index;
+    }
+
+    public Object getValue(Object ctx, Object elCtx, VariableResolverFactory vars) {
+        if (nextNode != null) {
+            return nextNode.getValue(Array.get(ctx, index), elCtx, vars);
+        }
+        else {
+            return Array.get(ctx, index);
+        }
+    }
+
+
+    public Object setValue(Object ctx, Object elCtx, VariableResolverFactory variableFactory, Object value) {
+        Array.set(ctx, index, value);
+        return value;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public AccessorNode getNextNode() {
+        return nextNode;
+    }
+
+    public AccessorNode setNextNode(AccessorNode nextNode) {
+        return this.nextNode = nextNode;
+    }
+
+
+    public String toString() {
+        return "Array Accessor -> [" + index + "]";
+    }
+}
