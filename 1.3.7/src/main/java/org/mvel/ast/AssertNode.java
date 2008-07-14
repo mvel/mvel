73698b964959a5f87eff984075ba33384cb2b28f
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2008-07-14 22:27:03 +0000

    MVEL 1.3.7 final

---
+++ 1.3.7/src/main/java/org/mvel/ast/AssertNode.java
@@ -0,0 +1,46 @@
+package org.mvel.ast;
+
+import org.mvel.ASTNode;
+import org.mvel.CompileException;
+import org.mvel.ExecutableStatement;
+import org.mvel.MVEL;
+import org.mvel.integration.VariableResolverFactory;
+import org.mvel.util.ParseTools;
+
+/**
+ * @author Christopher Brock
+ */
+public class AssertNode extends ASTNode {
+    public ExecutableStatement assertion;
+
+    public AssertNode(char[] expr, int fields) {
+        super(expr, fields);
+
+        if ((fields & COMPILE_IMMEDIATE) != 0) {
+            assertion = (ExecutableStatement) ParseTools.subCompileExpression(expr);
+        }
+    }
+
+    public Object getReducedValueAccelerated(Object ctx, Object thisValue, VariableResolverFactory factory) {
+        try {
+            Boolean bool = (Boolean) assertion.getValue(ctx, thisValue, factory);
+            if (!bool) throw new AssertionError("assertion failed in expression: " + new String(this.name));
+            return bool;
+        }
+        catch (ClassCastException e) {
+            throw new CompileException("assertion does not contain a boolean statement");
+        }
+    }
+
+    public Object getReducedValue(Object ctx, Object thisValue, VariableResolverFactory factory) {
+        try {
+            Boolean bool = (Boolean) MVEL.eval(this.name, ctx, factory);
+            if (!bool) throw new AssertionError("assertion failed in expression: " + new String(this.name));
+            return bool;
+        }
+        catch (ClassCastException e) {
+            throw new CompileException("assertion does not contain a boolean statement");
+        }
+        //   return getReducedValueAccelerated(ctx, thisValue, factory);
+    }
+}
