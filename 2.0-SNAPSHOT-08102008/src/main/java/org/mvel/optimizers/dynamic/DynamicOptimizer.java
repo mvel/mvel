b147202286f2330e8dfa871e2ef314eec7e0bc3e
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2008-08-10 18:23:54 +0000

    [maven-release-plugin]  copy for tag 2.0-SNAPSHOT-08102008

---
+++ 2.0-SNAPSHOT-08102008/src/main/java/org/mvel/optimizers/dynamic/DynamicOptimizer.java
@@ -0,0 +1,81 @@
+package org.mvel.optimizers.dynamic;
+
+import org.mvel.SetAccessor;
+import org.mvel.compiler.Accessor;
+import org.mvel.integration.VariableResolverFactory;
+import org.mvel.optimizers.AbstractOptimizer;
+import org.mvel.optimizers.AccessorOptimizer;
+import static org.mvel.optimizers.OptimizerFactory.SAFE_REFLECTIVE;
+import static org.mvel.optimizers.OptimizerFactory.getAccessorCompiler;
+import static org.mvel.optimizers.impl.asm.ASMAccessorOptimizer.setMVELClassLoader;
+
+import static java.lang.Thread.currentThread;
+
+public class DynamicOptimizer extends AbstractOptimizer implements AccessorOptimizer {
+    private AccessorOptimizer firstStage = getAccessorCompiler(SAFE_REFLECTIVE);
+
+    public static DynamicClassLoader classLoader;
+    public static int tenuringThreshold = 20;
+    public static long timeSpan = 150; // 1/2 second
+    public static int maximumTenure = 1;
+    public static int totalRecycled = 0;
+
+    public void init() {
+        _init();
+    }
+
+    private static void _init() {
+        setMVELClassLoader(classLoader = new DynamicClassLoader(currentThread().getContextClassLoader(), maximumTenure));
+    }
+
+    public static void enforceTenureLimit() {
+        if (classLoader.isOverloaded()) {
+            classLoader.deoptimizeAll();
+            totalRecycled =+ classLoader.getTotalClasses();
+            _init();
+        }
+    }
+
+    public static final int REGULAR_ACCESSOR = 0;
+
+    public Accessor optimizeAccessor(char[] property, Object ctx, Object thisRef, VariableResolverFactory factory, boolean rootThisRef) {
+        return classLoader.registerDynamicAccessor(new DynamicAccessor(property, 0, firstStage.optimizeAccessor(property, ctx, thisRef, factory, rootThisRef)));
+    }
+
+    public static final int SET_ACCESSOR = 1;
+
+    public SetAccessor optimizeSetAccessor(char[] property, Object ctx, Object thisRef, VariableResolverFactory factory, boolean rootThisRef, Object value) {
+        return firstStage.optimizeSetAccessor(property, ctx, thisRef, factory, rootThisRef, value);
+    }
+
+    public static final int COLLECTION = 2;
+
+    public Accessor optimizeCollection(char[] property, Object ctx, Object thisRef, VariableResolverFactory factory) {
+        return classLoader.registerDynamicAccessor(new DynamicAccessor(property, 2, firstStage.optimizeCollection(property, ctx, thisRef, factory)));
+    }
+
+    public static final int OBJ_CREATION = 3;
+
+    public Accessor optimizeObjectCreation(char[] property, Object ctx, Object thisRef, VariableResolverFactory factory) {
+        return classLoader.registerDynamicAccessor(new DynamicAccessor(property, 3, firstStage.optimizeObjectCreation(property, ctx, thisRef, factory)));
+    }
+
+    public static final int FOLD = 4;
+
+    public Accessor optimizeFold(char[] property, Object ctx, Object thisRef, VariableResolverFactory factory) {
+        enforceTenureLimit();
+        return classLoader.registerDynamicAccessor(new DynamicAccessor(property, 4, firstStage.optimizeFold(property, ctx, thisRef, factory)));
+    }
+
+    public Object getResultOptPass() {
+        return firstStage.getResultOptPass();
+    }
+
+    public Class getEgressType() {
+        return firstStage.getEgressType();
+    }
+
+    public boolean isLiteralOnly() {
+        return firstStage.isLiteralOnly();
+    }
+}
---
+++ 2.0-SNAPSHOT-08102008/src/main/java/org/mvel/templates/res/CodeNode.java
@@ -0,0 +1,38 @@
+package org.mvel.templates.res;
+
+import org.mvel.MVEL;
+import org.mvel.integration.VariableResolverFactory;
+import org.mvel.templates.TemplateRuntime;
+import static org.mvel.util.ParseTools.subset;
+import org.mvel.util.StringAppender;
+
+public class CodeNode extends Node {
+    public CodeNode() {
+    }
+
+    public CodeNode(int begin, String name, char[] template, int start, int end) {
+        this.begin = begin;
+        this.name = name;
+        this.contents = subset(template, this.cStart = start, (this.end = this.cEnd = end) - start - 1);
+    }
+
+    public CodeNode(int begin, String name, char[] template, int start, int end, Node next) {
+        this.name = name;
+        this.begin = begin;
+        this.contents = subset(template, this.cStart = start, (this.end = this.cEnd = end) - start - 1);
+        this.next = next;
+    }
+
+    public Object eval(TemplateRuntime runtime, StringAppender appender, Object ctx, VariableResolverFactory factory) {
+        MVEL.eval(contents, ctx, factory);
+        return next != null ? next.eval(runtime, appender, ctx, factory) : null;
+    }
+
+    public boolean demarcate(Node terminatingNode, char[] template) {
+        return false;
+    }
+
+    public String toString() {
+        return "CodeNode:" + name + "{" + (contents == null ? "" : new String(contents)) + "} (start=" + begin + ";end=" + end + ")";
+    }
+}
