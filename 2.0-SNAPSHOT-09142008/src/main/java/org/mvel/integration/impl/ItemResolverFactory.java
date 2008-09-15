0f63ec73d8bfaea8abd095895e9d5a3003a049b1
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2008-09-15 00:40:38 +0000

    [maven-release-plugin]  copy for tag 2.0-SNAPSHOT-09142008

---
+++ 2.0-SNAPSHOT-09142008/src/main/java/org/mvel/integration/impl/ItemResolverFactory.java
@@ -0,0 +1,81 @@
+package org.mvel.integration.impl;
+
+import org.mvel.integration.VariableResolver;
+import org.mvel.integration.VariableResolverFactory;
+
+
+public class ItemResolverFactory extends BaseVariableResolverFactory {
+    private final ItemResolver resolver;
+
+    public ItemResolverFactory(ItemResolver resolver, VariableResolverFactory nextFactory) {
+        this.resolver = resolver;
+        this.nextFactory = nextFactory;
+    }
+
+    public VariableResolver createVariable(String name, Object value) {
+        if (isTarget(name)) {
+            resolver.setValue(value);
+            return resolver;
+        }
+        else {
+            return nextFactory.createVariable(name, value);
+        }
+    }
+
+    public VariableResolver createVariable(String name, Object value, Class<?> type) {
+        if (isTarget(name)) {
+            // resolver.setValue(value);
+            throw new RuntimeException("variable already defined in scope: " + name);
+        }
+        else {
+            return nextFactory.createVariable(name, value);
+        }
+    }
+
+    public VariableResolver getVariableResolver(String name) {
+        return isTarget(name) ? resolver : nextFactory.getVariableResolver(name);
+    }
+
+    public boolean isTarget(String name) {
+        return resolver.getName().equals(name);
+    }
+
+    public boolean isResolveable(String name) {
+        return resolver.getName().equals(name) || (nextFactory != null && nextFactory.isResolveable(name));
+    }
+
+    public static class ItemResolver implements VariableResolver {
+        private final String name;
+        private Class type = Object.class;
+        private Object value;
+
+
+        public ItemResolver(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;  //To change body of implemented methods use File | Settings | File Templates.
+        }
+
+        public Class getType() {
+            return type;
+        }
+
+        public void setStaticType(Class type) {
+            this.type = type;
+        }
+
+        public int getFlags() {
+            return 0;
+        }
+
+        public Object getValue() {
+            return value;
+        }
+
+        public void setValue(Object value) {
+            this.value = value;
+        }
+    }
+}
