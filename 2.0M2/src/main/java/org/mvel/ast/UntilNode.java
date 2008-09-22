a7d1584a6b5cea57fd4a0190b263c29fd50dd29c
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2008-09-22 17:29:49 +0000

    [maven-release-plugin]  copy for tag 2.0M2

---
+++ 2.0M2/src/main/java/org/mvel/ast/UntilNode.java
@@ -0,0 +1,53 @@
+/**
+ * MVEL (The MVFLEX Expression Language)
+ *
+ * Copyright (C) 2007 Christopher Brock, MVFLEX/Valhalla Project and the Codehaus
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.mvel.ast;
+
+import org.mvel.compiler.ExecutableStatement;
+import org.mvel.integration.VariableResolverFactory;
+import static org.mvel.util.ParseTools.subCompileExpression;
+
+/**
+ * @author Christopher Brock
+ */
+public class UntilNode extends BlockNode {
+    protected String item;
+    protected ExecutableStatement condition;
+    protected ExecutableStatement compiledBlock;
+
+    public UntilNode(char[] condition, char[] block) {
+        this.condition = (ExecutableStatement) subCompileExpression(this.name = condition);
+        this.compiledBlock = (ExecutableStatement) subCompileExpression(this.block = block);
+    }
+
+    public Object getReducedValueAccelerated(Object ctx, Object thisValue, VariableResolverFactory factory) {
+        while (!(Boolean) condition.getValue(ctx, thisValue, factory)) {
+            compiledBlock.getValue(ctx, thisValue, factory);
+        }
+
+        return null;
+    }
+
+    public Object getReducedValue(Object ctx, Object thisValue, VariableResolverFactory factory) {
+        while (!(Boolean) condition.getValue(ctx, thisValue, factory)) {
+            compiledBlock.getValue(ctx, thisValue, factory);
+        }
+        return null;
+    }
+
+}
