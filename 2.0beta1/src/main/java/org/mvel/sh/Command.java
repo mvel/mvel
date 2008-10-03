d8a0d6c8d4927f13d9e6fd501ae8ebeae069070e
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2008-10-03 22:17:34 +0000

    delete tag.

--- 2.0beta1/src/main/java/org/mvel/sh/Command.java
+++
@@ -1,9 +0,0 @@
-package org.mvel.sh;
-
-import java.util.Map;
-
-public interface Command {
-    public Object execute(ShellSession session, String[] args);
-    public String getDescription();
-    public String getHelp();
-}
