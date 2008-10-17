19c532923df93695706faa0df704c339d6fba1ff
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2008-10-17 21:36:07 +0000

    update version

--- src/main/java/org/mvel2/MVEL.java
+++ src/main/java/org/mvel2/MVEL.java
@@ -40,7 +40,7 @@ import java.util.Map;
 public class MVEL {
     public static final String NAME = "MVEL (MVFLEX Expression Language)";
     public static final String VERSION = "2.0";
-    public static final String VERSION_SUB = "beta2";
+    public static final String VERSION_SUB = "RC";
     public static final String CODENAME = "enceladus";
 
     static boolean DEBUG_FILE = getBoolean("mvel2.debug.fileoutput");
