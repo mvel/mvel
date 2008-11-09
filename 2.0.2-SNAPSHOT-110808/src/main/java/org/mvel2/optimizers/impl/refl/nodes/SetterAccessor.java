5a7f250475ea3ffb8fb5d355826ea72b82b25d88
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2008-11-09 01:53:31 +0000

    [maven-release-plugin]  copy for tag 2.0.2-SNAPSHOT-110808

---
+++ 2.0.2-SNAPSHOT-110808/src/main/java/org/mvel2/optimizers/impl/refl/nodes/SetterAccessor.java
@@ -0,0 +1,68 @@
+package org.mvel2.optimizers.impl.refl.nodes;
+
+import org.mvel2.CompileException;
+import static org.mvel2.DataConversion.convert;
+import org.mvel2.compiler.AccessorNode;
+import org.mvel2.integration.VariableResolverFactory;
+
+import java.lang.reflect.Method;
+
+public class SetterAccessor implements AccessorNode {
+    private AccessorNode nextNode;
+    private final Method method;
+    private Class<?> targetType;
+
+    private boolean coercionRequired = false;
+
+    public static final Object[] EMPTY = new Object[0];
+
+    public Object setValue(Object ctx, Object elCtx, VariableResolverFactory variableFactory, Object value) {
+        try {
+            if (coercionRequired) {
+                return method.invoke(ctx, convert(value, targetType));
+            }
+            else {
+                return method.invoke(ctx, value);
+            }
+        }
+        catch (IllegalArgumentException e) {
+            if (!coercionRequired) {
+                coercionRequired = true;
+                return setValue(ctx, elCtx, variableFactory, value);
+            }
+            throw new CompileException("unable to bind property", e);
+        }
+        catch (Exception e) {
+            throw new CompileException("error binding property", e);
+        }
+    }
+
+    public Object getValue(Object ctx, Object elCtx, VariableResolverFactory vars) {
+        return null;
+    }
+
+    public SetterAccessor(Method method) {
+        this.method = method;
+        this.targetType = method.getParameterTypes()[0];
+    }
+
+    public Method getMethod() {
+        return method;
+    }
+
+    public AccessorNode setNextNode(AccessorNode nextNode) {
+        return this.nextNode = nextNode;
+    }
+
+    public AccessorNode getNextNode() {
+        return nextNode;
+    }
+
+    public String toString() {
+        return method.getDeclaringClass().getName() + "." + method.getName();
+    }
+
+    public Class getKnownEgressType() {
+        return method.getReturnType();
+    }
+}
