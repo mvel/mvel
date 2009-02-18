ea515777fd416587275ea33975175848e25c3905
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2009-02-18 21:55:03 +0000

    [maven-release-plugin]  copy for tag 2.0.7-SNAPSHOT-021809

---
+++ 2.0.7-SNAPSHOT-021809/src/main/java/org/mvel2/templates/util/TemplateTools.java
@@ -0,0 +1,115 @@
+/**
+ * MVEL 2.0
+ * Copyright (C) 2007 The Codehaus
+ * Mike Brock, Dhanji Prasanna, John Graham, Mark Proctor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mvel2.templates.util;
+
+import org.mvel2.templates.TemplateError;
+import org.mvel2.templates.res.Node;
+import org.mvel2.templates.res.TerminalNode;
+import static org.mvel2.util.ParseTools.balancedCapture;
+import org.mvel2.util.StringAppender;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+import static java.nio.ByteBuffer.allocateDirect;
+import java.nio.channels.FileChannel;
+
+public class TemplateTools {
+    public static Node getLastNode(Node node) {
+        Node n = node;
+        while (true) {
+            if (n.getNext() instanceof TerminalNode) return n;
+            n = n.getNext();
+        }
+    }
+
+    public static int captureToEOS(char[] expression, int cursor) {
+        int length = expression.length;
+        while (cursor != length) {
+            switch (expression[cursor]) {
+                case '(':
+                case '[':
+                case '{':
+                    cursor = balancedCapture(expression, cursor, expression[cursor]);
+                    break;
+
+                case ';':
+                case '}':
+                    return cursor;
+
+            }
+            cursor++;
+        }
+
+        return cursor;
+    }
+
+    public static String readInFile(String file) {
+        return readInFile(new File(file));
+    }
+
+    public static String readInFile(File file) {
+        try {
+            FileChannel fc = new FileInputStream(file).getChannel();
+            ByteBuffer buf = allocateDirect(10);
+            StringAppender appender = new StringAppender();
+            int read;
+
+            while (true) {
+                buf.rewind();
+                if ((read = fc.read(buf)) != -1) {
+                    buf.rewind();
+                    for (; read != 0; read--) {
+                        appender.append((char) buf.get());
+                    }
+                }
+                else {
+                    break;
+                }
+            }
+
+            fc.close();
+
+            return appender.toString();
+        }
+        catch (FileNotFoundException e) {
+            throw new TemplateError("cannot include template '" + file.getName() + "': file not found.");
+        }
+        catch (IOException e) {
+            throw new TemplateError("unknown I/O exception while including '" + file.getName() + "' (stacktrace nested)", e);
+        }
+    }
+
+    public static String readStream(InputStream instream) {
+        try {
+            byte[] buf = new byte[10];
+            StringAppender appender = new StringAppender();
+            int read;
+            while ((read = instream.read(buf)) != -1) {
+                for (int i = 0; i < read; i++) {
+                    appender.append((char) buf[i]);
+                }
+            }
+
+            return appender.toString();
+        }
+        catch (IOException e) {
+            throw new TemplateError("unknown I/O exception while including (stacktrace nested)", e);
+        }
+    }
+}
---
+++ 2.0.7-SNAPSHOT-021809/src/main/java/org/mvel2/util/CollectionParser.java
@@ -0,0 +1,249 @@
+/**
+ * MVEL 2.0
+ * Copyright (C) 2007 The Codehaus
+ * Mike Brock, Dhanji Prasanna, John Graham, Mark Proctor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mvel2.util;
+
+import org.mvel2.CompileException;
+import org.mvel2.DataConversion;
+import org.mvel2.compiler.ExecutableStatement;
+import static org.mvel2.util.ParseTools.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * This is the inline collection sub-parser.  It produces a skeleton model of the collection which is in turn translated
+ * into a sequenced AST to produce the collection efficiently at runtime, and passed off to one of the JIT's if
+ * configured.
+ *
+ * @author Christopher Brock
+ */
+public class CollectionParser {
+    private char[] property;
+
+    private int cursor;
+    private int length;
+    private int start;
+
+    private int type;
+
+    public static final int LIST = 0;
+    public static final int ARRAY = 1;
+    public static final int MAP = 2;
+
+    private Class colType;
+    private boolean strongType;
+
+    private static final Object[] EMPTY_ARRAY = new Object[0];
+
+    public CollectionParser() {
+    }
+
+    public CollectionParser(int type) {
+        this.type = type;
+    }
+
+    public Object parseCollection(char[] property, boolean subcompile, boolean strongType) {
+        this.cursor = 0;
+        this.strongType = strongType;
+        if ((this.length = (this.property = property).length) > 0)
+            while (length > 0 && isWhitespace(property[length - 1]))
+                length--;
+
+        return parseCollection(subcompile);
+    }
+
+    public Object parseCollection(char[] property, boolean subcompile, Class colType, boolean strongType) {
+        if (colType != null) this.colType = getBaseComponentType(colType);
+        this.cursor = 0;
+        this.strongType = strongType;
+        if ((this.length = (this.property = property).length) > 0)
+            while (length > 0 && isWhitespace(property[length - 1]))
+                length--;
+
+        return parseCollection(subcompile);
+    }
+
+    private Object parseCollection(boolean subcompile) {
+        if (length == 0) {
+            if (type == LIST) return new ArrayList();
+            else return EMPTY_ARRAY;
+        }
+
+        Map<Object, Object> map = null;
+        List<Object> list = null;
+        String ex;
+
+        if (type != -1) {
+            switch (type) {
+                case ARRAY:
+                case LIST:
+                    list = new ArrayList<Object>();
+                    break;
+                case MAP:
+                    map = new HashMap<Object, Object>();
+                    break;
+            }
+        }
+
+        Object curr = null;
+        int newType = -1;
+
+        for (; cursor < length; cursor++) {
+            switch (property[cursor]) {
+                case '{':
+                    if (newType == -1) {
+                        newType = ARRAY;
+                    }
+
+                case '[':
+                    if (cursor > 0 && isIdentifierPart(property[cursor - 1])) continue;
+
+                    if (newType == -1) {
+                        newType = LIST;
+                    }
+
+                    /**
+                     * Sub-parse nested collections.
+                     */
+                    Object o = new CollectionParser(newType).parseCollection(subset(property, (start = cursor) + 1,
+                            cursor = balancedCapture(property, start, property[start])), subcompile, colType, strongType);
+
+                    if (type == MAP) {
+                        map.put(curr, o);
+                    }
+                    else {
+                        list.add(curr = o);
+                    }
+
+
+                    if ((start = ++cursor) < (length - 1) && property[cursor] == ',') {
+                        start = cursor + 1;
+                    }
+
+                    continue;
+
+                case '(':
+                    cursor = balancedCapture(property, cursor, '(');
+
+                    break;
+
+                case '\"':
+                case '\'':
+                    cursor = balancedCapture(property, cursor, property[cursor]);
+
+                    break;
+
+                case ',':
+                    if (type != MAP) {
+                        list.add(ex = new String(property, start, cursor - start));
+                    }
+                    else {
+                        map.put(curr, ex = createStringTrimmed(property, start, cursor - start));
+                    }
+
+                    if (subcompile) {
+                        subCompile(ex);
+                    }
+
+                    start = cursor + 1;
+
+                    break;
+
+                case ':':
+                    if (type != MAP) {
+                        map = new HashMap<Object, Object>();
+                        type = MAP;
+                    }
+                    curr = createStringTrimmed(property, start, cursor - start);
+
+                    if (subcompile) {
+                        subCompile((String) curr);
+                    }
+
+                    start = cursor + 1;
+                    break;
+
+                case '.':
+                    cursor++;
+                    while (cursor != length && isWhitespace(property[cursor])) cursor++;
+                    if (cursor != length && property[cursor] == '{') {
+                        cursor = balancedCapture(property, cursor, '{');
+                    }
+                    break;
+            }
+        }
+
+        if (start < length) {
+            if (cursor < (length - 1)) cursor++;
+
+            if (type == MAP) {
+                map.put(curr, ex = createStringTrimmed(property, start, cursor - start));
+            }
+            else {
+                if (cursor < length) cursor++;
+                list.add(ex = createStringTrimmed(property, start, cursor - start));
+            }
+
+            if (subcompile) subCompile(ex);
+        }
+
+        switch (type) {
+            case MAP:
+                return map;
+            case ARRAY:
+                return list.toArray();
+            default:
+                return list;
+        }
+    }
+
+    private void subCompile(String ex) {
+        if (colType == null) {
+            subCompileExpression(ex.toCharArray());
+        }
+        else {
+            Class r = ((ExecutableStatement) subCompileExpression(ex.toCharArray())).getKnownEgressType();
+            if (!colType.isAssignableFrom(r) && (strongType || !DataConversion.canConvert(r, colType))) {
+                 throw new CompileException("expected type: " + colType.getName() + "; but found: " + r.getName());
+            }
+        }
+    }
+
+    private static char[] subset(char[] property, int start, int end) {
+        while (start < (end - 1) && isWhitespace(property[start]))
+            start++;
+
+        char[] newA = new char[end - start];
+        int i = 0;
+        while (i != newA.length) {
+            newA[i] = property[i + start];
+            i++;
+        }
+
+        return newA;
+    }
+
+
+    public int getCursor() {
+        return cursor;
+    }
+
+}
