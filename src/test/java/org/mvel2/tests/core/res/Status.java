0d899c6129515c6a43ecc361152e8dce31675801
Mark Proctor <mdproctor@gmail.com>
2011-06-22 21:59:21 +0100

    corrected package imports with enums test

--- src/test/java/org/mvel2/tests/core/res/Status.java
+++ src/test/java/org/mvel2/tests/core/res/Status.java
@@ -3,6 +3,37 @@ package org.mvel2.tests.core.res;
 /**
 * @author Mike Brock <cbrock@redhat.com>
 */
-public enum Status {
-  Ready
+public class Status {
+    public static final int START = 0;
+    public static final int STOP = 0;
+    
+    private int value;
+    
+    public Status(int value) {
+        this.value = value;
+    }
+    
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + value;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if ( this == obj ) return true;
+        if ( obj == null ) return false;
+        if ( getClass() != obj.getClass() ) return false;
+        Status other = (Status) obj;
+        if ( value != other.value ) return false;
+        return true;
+    }
+    
+    
 }
