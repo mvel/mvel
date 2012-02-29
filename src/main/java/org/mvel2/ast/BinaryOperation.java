318304dda4acd12b1fb38f80b612e9a5c8329070
mariofusco <mario.fusco@gmail.com>
2012-02-29 12:19:42 +0100

    make class assignability check bidiractional

--- src/main/java/org/mvel2/ast/BinaryOperation.java
+++ src/main/java/org/mvel2/ast/BinaryOperation.java
@@ -80,7 +80,7 @@ public class BinaryOperation extends BooleanNode {
       default:
         if (!ctx.isStrongTyping()) break;
 
-        if (!left.getEgressType().isAssignableFrom(right.getEgressType())) {
+        if (!left.getEgressType().isAssignableFrom(right.getEgressType()) && !right.getEgressType().isAssignableFrom(left.getEgressType())) {
           if (right.isLiteral() && canConvert(left.getEgressType(), right.getEgressType())) {
             this.right = new LiteralNode(convert(right.getReducedValueAccelerated(null, null, null), left.getEgressType()));
           } else if (!right.getEgressType().equals(Object.class)
