2d914fb1f13c1ec337cbc31590dfcdf651b67d5f
Mario Fusco <mario.fusco@gmail.com>
2015-05-14 19:08:58 +0200

    fix constructor varargs invocation

--- src/main/java/org/mvel2/optimizers/impl/refl/nodes/MethodAccessor.java
+++ src/main/java/org/mvel2/optimizers/impl/refl/nodes/MethodAccessor.java
@@ -18,25 +18,17 @@
  */
 package org.mvel2.optimizers.impl.refl.nodes;
 
-import org.mvel2.compiler.AccessorNode;
 import org.mvel2.compiler.ExecutableStatement;
 import org.mvel2.integration.VariableResolverFactory;
 
-import java.lang.reflect.Array;
 import java.lang.reflect.Method;
 
-import static org.mvel2.DataConversion.convert;
 import static org.mvel2.util.ParseTools.getBestCandidate;
 import static org.mvel2.util.ParseTools.getWidenedTarget;
 
-public class MethodAccessor implements AccessorNode {
-  private AccessorNode nextNode;
+public class MethodAccessor extends InvokableAccessor {
 
   private Method method;
-  private Class[] parameterTypes;
-  private ExecutableStatement[] parms;
-  private int length;
-  private boolean coercionNeeded = false;
 
   public Object getValue(Object ctx, Object elCtx, VariableResolverFactory vars) {
     if (!coercionNeeded) {
@@ -157,29 +149,6 @@ public class MethodAccessor implements AccessorNode {
     return vals;
   }
 
-  private Object[] executeAndCoerce(Class[] target, Object elCtx, VariableResolverFactory vars, boolean isVarargs) {
-    Object[] values = new Object[length];
-    for (int i = 0; i < length && !(isVarargs && i >= length-1); i++) {
-      //noinspection unchecked
-      values[i] = convert(parms[i].getValue(elCtx, vars), target[i]);
-    }
-    if (isVarargs) {
-        Class<?> componentType = target[length-1].getComponentType();
-        Object vararg;
-        if(parms == null){
-            vararg = Array.newInstance(componentType, 0);
-        }else{
-
-            vararg = Array.newInstance(componentType, parms.length - length + 1);
-            for (int i = length-1; i < parms.length; i++) {
-            Array.set(vararg, i - length + 1, convert(parms[i].getValue(elCtx, vars), componentType));
-        }
-        }
-        values[length-1] = vararg;
-    }
-    return values;
-  }
-
   public Method getMethod() {
     return method;
   }
@@ -205,14 +174,6 @@ public class MethodAccessor implements AccessorNode {
     this.parms = parms;
   }
 
-  public AccessorNode getNextNode() {
-    return nextNode;
-  }
-
-  public AccessorNode setNextNode(AccessorNode nextNode) {
-    return this.nextNode = nextNode;
-  }
-
   public Object setValue(Object ctx, Object elCtx, VariableResolverFactory variableFactory, Object value) {
     try {
       return nextNode.setValue(method.invoke(ctx, executeAll(elCtx, variableFactory, method)), elCtx, variableFactory, value);
@@ -235,10 +196,6 @@ public class MethodAccessor implements AccessorNode {
 
   public Class getKnownEgressType() {
     return method.getReturnType();
-  }
-
-  public Class[] getParameterTypes() {
-      return parameterTypes;
   }
 }
 
