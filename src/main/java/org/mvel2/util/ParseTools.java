41f08fa8589b1baa241a40993d150bbca246193f
Ken Scoggins <kscoggins@synapsense.com>
2015-07-09 11:48:37 -0700

    Fixed bug where handleNumericConversion was expecting the old API style for the val arg when the literal was hex or oct.

--- src/main/java/org/mvel2/util/ParseTools.java
+++ src/main/java/org/mvel2/util/ParseTools.java
@@ -1642,7 +1642,7 @@ public class ParseTools {
         }
       }
 
-      return Integer.decode(new String(val));
+      return Integer.decode(new String(val, start, offset));
     }
     else if (!isDigit(val[start + offset - 1])) {
       switch (val[start + offset - 1]) {
