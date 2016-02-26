2888592d7c42074f45024a8e66dffbcb6bb419e0
Frank-Michael Moser <moser@decodon.com>
2016-02-26 17:52:42 +0100

    added command to print the current environment variables (similar to showvars)

---
+++ src/main/java/org/mvel2/sh/command/basic/ShowEnv.java
@@ -0,0 +1,48 @@
+/**
+ * MVEL 2.0
+ * Copyright (C) 2007 The Codehaus
+ * Mike Brock, Dhanji Prasanna, John Graham, Mark Proctor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mvel2.sh.command.basic;
+
+import org.mvel2.sh.Command;
+import org.mvel2.sh.ShellSession;
+
+import java.util.Map;
+
+public class ShowEnv implements Command {
+
+  public Object execute(ShellSession session, String[] args) {
+
+    Map<String, String> env = session.getEnv();
+
+    System.out.println("Printing Environment ...");
+    for (String key : env.keySet()) {
+      System.out.println(key + " => " + env.get(key));
+    }
+
+    System.out.println(" ** " + env.size() + " environment variables total.");
+
+    return null;
+  }
+
+  public String getDescription() {
+    return "shows current environment";
+  }
+
+  public String getHelp() {
+    return "no help yet";
+  }
+}
