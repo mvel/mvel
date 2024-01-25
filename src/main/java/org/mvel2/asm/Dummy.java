12f42c60a92178815a50301eb9a28f692ba1ca0b
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2024-01-25 15:10:19 +0900

    [DROOLS-7604] kie-karaf-itests fail with "Unable to resolve org.mvel2/2.5.1.Final" - Add org/mvel2/asm/Dummy.java to create "org.mvel2.asm" entry in "Export-Package" in MANIFEST.MF

---
+++ src/main/java/org/mvel2/asm/Dummy.java
@@ -0,0 +1,10 @@
+package org.mvel2.asm;
+
+/**
+ * Dummy class to generate a "org.mvel2.asm" package entry in MANIFEST.MF "Export-Package:"
+ * <p>
+ * See https://issues.redhat.com/browse/DROOLS-7604
+ */
+public class Dummy {
+
+}
