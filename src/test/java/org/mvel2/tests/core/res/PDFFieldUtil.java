7d24a25937ef89a1c9b1b472048e99f281e832c3
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2024-01-25 19:35:30 +0900

    [mvel-352] ControlFlowTests.testCalculateAge fails - Fix test not to depend on System.currentTimeMillis()

--- src/test/java/org/mvel2/tests/core/res/PDFFieldUtil.java
+++ src/test/java/org/mvel2/tests/core/res/PDFFieldUtil.java
@@ -5,9 +5,13 @@ import java.util.Date;
 
 public class PDFFieldUtil {
   public int calculateAge(Date gebDat) {
-    long milliAge = System.currentTimeMillis() - gebDat.getTime();
+    Calendar c2 = Calendar.getInstance();
+    c2.set(2023,
+           11,
+           25); // 2023 dec 25
+    long milliAge = c2.getTimeInMillis() - gebDat.getTime();
     Calendar geburtsTag = Calendar.getInstance();
     geburtsTag.setTimeInMillis(milliAge);
-    return geburtsTag.get(Calendar.YEAR) - 1970;
+    return geburtsTag.get(Calendar.YEAR) - 1970; // get age of a person who was born in gebDat on 2023 dec 25
   }
 }
