10b0a0de933e9d64c8c95038a5202dca1d4159bf
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2026-04-27 11:34:09 +0900

    add ReadPropsFixture POJO for read-properties tests
    Refs mvel/mvel#423

---
+++ src/test/java/org/mvel3/transpiler/ReadPropsFixture.java
@@ -0,0 +1,16 @@
+package org.mvel3.transpiler;
+
+public class ReadPropsFixture {
+    private int salary;
+    private int basePay;
+    private boolean active;
+
+    public int getSalary() { return salary; }
+    public void setSalary(int salary) { this.salary = salary; }
+
+    public int getBasePay() { return basePay; }
+    public void setBasePay(int basePay) { this.basePay = basePay; }
+
+    public boolean isActive() { return active; }
+    public void setActive(boolean active) { this.active = active; }
+}
