0438102687bb429999a0b5e2b8f9788743ab92d0
Yeser Amer <yeser.amer@ibm.com>
2026-09-25 09:03:54 +0200

    Change Request

--- src/main/java/org/mvel2/util/Make.java
+++ src/main/java/org/mvel2/util/Make.java
@@ -144,6 +144,13 @@ public class Make {
       this.listInstance = listInstance;
     }
 
+    /**
+     * Adds a value to this Set builder.
+     * Renamed from {@code _}: {@code _} is a reserved keyword since Java 9.
+     *
+     * @param value the value to add
+     * @return this builder
+     */
     public Set<V> _add(V value) {
       listInstance.add(value);
       return this;
